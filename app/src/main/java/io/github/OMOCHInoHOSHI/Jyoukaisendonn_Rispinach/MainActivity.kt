eb9d4fdf570b3bfaa560a2f6d6a7d1fa1e20e982
OMOCHInoHOSHI <166191100+omochinohoshi@users.noreply.github.com>
2024-10-18 16:01:33 +0900

    Python導入(n_a)

--- app/src/main/java/io/github/OMOCHInoHOSHI/Jyoukaisendonn_Rispinach/MainActivity.kt
+++ app/src/main/java/io/github/OMOCHInoHOSHI/Jyoukaisendonn_Rispinach/MainActivity.kt
@@ -11,17 +11,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.chaquo.python.Python
 import io.github.OMOCHInoHOSHI.Jyoukaisendonn_Rispinach.ui.theme.RispinachTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Pythonにより追加
+        val py = Python.getInstance()
+        val module = py.getModule("jikken")
+        val tex1 = module.callAttr("hello_world")
+        println(tex1)
+        
         enableEdgeToEdge()
         setContent {
             RispinachTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "print_py",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
