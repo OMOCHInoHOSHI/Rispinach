42cf056ebc860cf88f7d1d707333e16d2a1d981b
KazuhoIguchi <kd1317629@st.kobedenshi.ac.jp>
2025-01-07 11:11:50 +0900

    チャット機能の仮実装

--- app/src/main/java/io/github/OMOCHInoHOSHI/Jyoukaisendonn_Rispinach/Chat.kt
+++ app/src/main/java/io/github/OMOCHInoHOSHI/Jyoukaisendonn_Rispinach/Chat.kt
@@ -1,6 +1,151 @@
 package io.github.OMOCHInoHOSHI.Jyoukaisendonn_Rispinach
 
-fun Chat()
-{
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@Composable
+fun Conversation(messages: List<Message>, modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier.padding(bottom = 1.dp) // 必要に応じてパディングを調整
+    ) {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Composable
+fun MessageCard(msg: Message) {
+    var isExpanded by remember { mutableStateOf(false) }
+    val surfaceColor by animateColorAsState(
+        if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+    )
+
+    Row(
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .clickable { isExpanded = !isExpanded }
+    ) {
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MessageInput(
+    text: String,
+    onTextChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row()
+    {
+        OutlinedTextField(
+            value = text,
+            onValueChange = onTextChange,
+            modifier = modifier
+                //.fillMaxWidth()
+                .width(300.dp)
+                .padding(8.dp),
+            maxLines = 5,
+            singleLine = false,
+            label = { Text("メッセージを入力") }
+        )
+
+        //投稿ボタン
+        IconButton(
+            modifier = Modifier
+                .align(alignment = Alignment.CenterVertically),
+            onClick = {/*投稿処理*/}
+        )
+        {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.Send,
+                contentDescription = "投稿",
+                modifier = Modifier
+                    .size(100.dp),
+                //.border(2.dp, Color.White, RoundedCornerShape(20.dp)),
+                tint = Color(0xFF2196F3)
+            )
+        }
+    }
+}
+
+@Composable
+fun OutlinedText(
+    text: String,
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle = TextStyle.Default,
+    stroke: Stroke = Stroke(),
+    strokeColor: Color = Color.Transparent,
+) {
+    var textLayoutResult: TextLayoutResult? by remember { mutableStateOf(null) }
+    BasicText(
+        text = text,
+        style = textStyle,
+        onTextLayout = { textLayoutResult = it },
+        modifier = modifier
+            .padding(4.dp)
+            .drawBehind {
+                textLayoutResult?.let {
+                    drawText(
+                        textLayoutResult = it,
+                        color = strokeColor,
+                        //style = textStyle.copy(drawStyle = stroke)
+                    )
+                }
+            }
+    )
 }
--- app/src/main/java/io/github/OMOCHInoHOSHI/Jyoukaisendonn_Rispinach/Posts.kt
+++ app/src/main/java/io/github/OMOCHInoHOSHI/Jyoukaisendonn_Rispinach/Posts.kt
@@ -2,158 +2,112 @@ package io.github.OMOCHInoHOSHI.Jyoukaisendonn_Rispinach
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Posts(pName:Int,lName:String,cName:String)
-{
-    Box(
-        modifier = Modifier
-            .fillMaxWidth(),
-        contentAlignment=Alignment.Center
-        //            modifier = Modifier
-//                .align(Alignment.TopCenter)
-    )
-    {
-        Image(
-            painter = painterResource(pName),
-            contentDescription = "test",
-            //
-//            modifier = Modifier
-//                .align(Alignment.TopCenter)
-        )
-        Box(
-            modifier = Modifier
-                .align(Alignment.BottomStart),
-        )
-        {
-            Column()
-            {
-                OutlinedText(
-                    text = "名前:$lName",
-                    stroke = Stroke(width = 4.0f),
-                    textStyle = TextStyle(
-                        fontSize = 20.sp
-                    ),
-                    strokeColor = Color.White
-                )
+fun Posts(pName: Int, lName: String, cName: String) {
+    val scope = rememberCoroutineScope()
+    var openBottomSheet by remember { mutableStateOf(false) }
+    val bottomSheetState = rememberModalBottomSheetState()
 
-            }
-        }
-
-        Box(
-            modifier = Modifier
-            .align(Alignment.BottomEnd),//iマークの位置設定
-            )
-        {
-            var skipPartiallyExpanded by rememberSaveable { mutableStateOf(false) }
-            val scope = rememberCoroutineScope()
-            val bottomSheetState = rememberModalBottomSheetState(
-                skipPartiallyExpanded = skipPartiallyExpanded
-            )
-            var openBottomSheet by remember { mutableStateOf(false) }
-            IconButton(
-                onClick = {
-                    openBottomSheet = true
-                }
-            )
-            {
-
-
-                Icon(
-                    imageVector = Icons.Filled.Info,
-                    contentDescription = "Info",
+    Scaffold(
+        topBar = {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.TopCenter
+            ) {
+                Image(
+                    painter = painterResource(id = pName),
+                    contentDescription = "プロフィール画像",
                     modifier = Modifier
-                        .border(2.dp, Color.White, RoundedCornerShape(20.dp)),
-                    tint= Color(0xFFB0BEC5))
-                if (openBottomSheet)
-                {
-                    ModalBottomSheet(
-                        //modifier = Modifier.padding(top = 16.dp),
-                        onDismissRequest = {openBottomSheet = false },
-                        sheetState = bottomSheetState,
+                        .fillMaxWidth()
+                        .height(200.dp) // 必要に応じて高さを調整
+                )
+                Box(
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                        .padding(8.dp)
+                ) {
+                    OutlinedText(
+                        text = "名前: $lName",
+                        stroke = Stroke(width = 4.0f),
+                        textStyle = TextStyle(fontSize = 20.sp),
+                        strokeColor = Color.White
                     )
-                    {
-                        Column(
+                }
+                Box(
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .padding(8.dp)
+                ) {
+                    IconButton(onClick = { openBottomSheet = true }) {
+                        Icon(
+                            imageVector = Icons.Filled.Info,
+                            contentDescription = "情報",
                             modifier = Modifier
+                                .border(2.dp, Color.White, RoundedCornerShape(20.dp)),
+                            tint = Color(0xFFB0BEC5)
                         )
-                        {
-                            Row()
-                            {
-                                Text("場所:")
-                                Text(
-                                    text = cName,
-                                    textDecoration = TextDecoration.Underline,
-                                    color = Color.Blue
-                                    )
-                            }
+                    }
+                }
+            }
+        },
+        content = { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Conversation(
+                        messages = SampleData.conversationSample,
+                        modifier = Modifier.weight(1f)
+                    )
+                    var text by rememberSaveable { mutableStateOf("") }
+                    MessageInput(
+                        text = text,
+                        onTextChange = { text = it }
+                    )
+                }
+                if (openBottomSheet) {
+                    ModalBottomSheet(
+                        onDismissRequest = { openBottomSheet = false },
+                        sheetState = bottomSheetState,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Column(modifier = Modifier.padding(16.dp)) {
+                            Text("場所:", style = MaterialTheme.typography.titleMedium)
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = cName,
+                                textDecoration = TextDecoration.Underline,
+                                color = Color.Blue,
+                                style = MaterialTheme.typography.bodyLarge,
+                                modifier = Modifier.clickable { /* 必要に応じてクリック処理を追加 */ }
+                            )
                         }
                     }
                 }
             }
-
         }
-    }
-}
-
-@Composable
-fun OutlinedText(
-    text: String,
-    modifier: Modifier = Modifier,
-    textStyle: TextStyle = TextStyle.Default,
-    stroke: Stroke = Stroke(),
-    strokeColor: Color = Color.Transparent,
-) {
-    var textLayoutResult: TextLayoutResult? by remember {
-        mutableStateOf(null)
-    }
-    Text(
-        text = text,
-        style = textStyle,
-        onTextLayout = {
-            textLayoutResult = it
-        },
-        modifier = modifier
-            .drawBehind {
-                textLayoutResult?.let {
-                    drawText(
-                        textLayoutResult = it,
-                        drawStyle = stroke,
-                        color = strokeColor,
-                    )
-                }
-            }
     )
 }
