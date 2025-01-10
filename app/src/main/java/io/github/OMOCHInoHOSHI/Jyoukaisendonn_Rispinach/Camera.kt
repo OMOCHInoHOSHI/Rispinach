8c345f8cfaa49e1d794129b94b02054247adc158
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-01-10 11:33:35 +0900

    カメラからHomeに戻る（選択バグはそのまま）

--- app/src/main/java/io/github/OMOCHInoHOSHI/Jyoukaisendonn_Rispinach/Camera.kt
+++ app/src/main/java/io/github/OMOCHInoHOSHI/Jyoukaisendonn_Rispinach/Camera.kt
@@ -1,25 +1,75 @@
 package io.github.OMOCHInoHOSHI.Jyoukaisendonn_Rispinach
 
 import android.util.Log
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.zIndex
+import androidx.navigation.NavGraphBuilder
 
 @Composable
 fun Camera()
 {
-    var camera_flg = 1
-    camera_flg = CameraScreen_2(1)
+    var isCameraOpen by remember { mutableStateOf(false) }
 
-//    SideEffect { Log.d("compose-log", "Camera") }
-//    Column()
-//    {
-////        Button()
-////        {
-////
-////        }
+    var camera_flg = 1
+//    camera_flg = CameraScreen_2(1)
+//    Popup(alignment = Alignment.Center) {
+//        Box(
+//            modifier = Modifier
+//                .fillMaxSize()
+//        ) {
+//            camera_flg = CameraScreen_2(1)
+//        }
+//    }
+    var showCamera by remember { mutableStateOf(true) } // カメラ表示フラグ
+
+    if (showCamera) {
+
+//        BackHandler {
+//            println("BackHandler")
+//            showCamera = false // カメラ画面を閉じる
+//        }
+
+        Popup(alignment = Alignment.Center) {
+            // バックボタン処理を追加
+            BackHandler {
+                println("BackHandler")
+                showCamera = false // カメラ画面を閉じる
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+            camera_flg = CameraScreen_2(1)
+                BackHandler {
+                    println("BackHandler")
+                    showCamera = false // カメラ画面を閉じる
+                }
+            }
+        }
+    } else {
+        // カメラ画面を閉じた後の処理（元の画面）
+        Home()
+
+    }
+
 //    }
 
 
