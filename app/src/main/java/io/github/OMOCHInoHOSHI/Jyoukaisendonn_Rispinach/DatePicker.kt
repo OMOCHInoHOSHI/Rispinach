74089e8bfededd85d5bbf42a760b5ce368264954
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-01-13 22:45:53 +0900

    日付選択ダイアログのコンポーネント作成

---
+++ app/src/main/java/io/github/OMOCHInoHOSHI/Jyoukaisendonn_Rispinach/DatePicker.kt
@@ -0,0 +1,39 @@
+package io.github.OMOCHInoHOSHI.Jyoukaisendonn_Rispinach
+
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+
+// 日付選択ダイアログのコンポーネントS-------------------------------------------------
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerModal(
+    onDateSelected: (Long?) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val datePickerState = rememberDatePickerState()
+
+    DatePickerDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = {
+                onDateSelected(datePickerState.selectedDateMillis)
+                onDismiss()
+            }) {
+                Text("OK")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("キャンセル")
+            }
+        }
+    ) {
+        DatePicker(state = datePickerState)
+    }
+}
+// 日付選択ダイアログのコンポーネントE-------------------------------------------------
