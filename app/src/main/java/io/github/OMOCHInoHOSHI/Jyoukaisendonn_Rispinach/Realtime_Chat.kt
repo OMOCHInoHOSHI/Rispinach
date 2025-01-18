1ceefc5aed7870c18081bf3986ea4c1c323262bc
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-01-18 17:41:58 +0900

    Realtime Database でキーだけを取得し、キーをメタデータとして保存

--- app/src/main/java/io/github/OMOCHInoHOSHI/Jyoukaisendonn_Rispinach/Realtime_Chat.kt
+++ app/src/main/java/io/github/OMOCHInoHOSHI/Jyoukaisendonn_Rispinach/Realtime_Chat.kt
@@ -1,6 +1,11 @@
 package io.github.OMOCHInoHOSHI.Jyoukaisendonn_Rispinach
 
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -21,6 +26,7 @@ class ChatRepository {
     }
 }
 
+// firebase realtime databaseにデータを書き込むS-----------
 fun Realtime_Database_Write_Message(){
     // Write a message to the database
     val database = Firebase.database
@@ -28,3 +34,13 @@ fun Realtime_Database_Write_Message(){
 
     myRef.setValue("こんにちは！！！！")
 }
+// firebase realtime databaseにデータを書き込むE-----------
+
+
+// Realtime Database でキーだけを取得S--------------------------------------------------------
+fun getNewKeyFromRealtimeDatabase(databaseReference: DatabaseReference): String {
+    val newChildRef = databaseReference.push()
+    val newKey = newChildRef.key ?: ""
+    return newKey
+}
+// Realtime Database でキーだけを取得E--------------------------------------------------------
