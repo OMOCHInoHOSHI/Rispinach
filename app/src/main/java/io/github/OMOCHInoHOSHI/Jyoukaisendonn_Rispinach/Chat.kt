ae32aad03187ffdc668ec8be664ff88d39915712
KazuhoIguchi <kd1317629@st.kobedenshi.ac.jp>
2025-01-19 23:34:28 +0900

    コメントと入力欄が被って入力できなくなる不具合を力技で調整

--- app/src/main/java/io/github/OMOCHInoHOSHI/Jyoukaisendonn_Rispinach/Chat.kt
+++ app/src/main/java/io/github/OMOCHInoHOSHI/Jyoukaisendonn_Rispinach/Chat.kt
@@ -179,8 +179,8 @@ fun MessageInput(text: String, onTextChange: (String) -> Unit, postId: String, m
             onValueChange = onTextChange,  // onTextChangeを渡す
             modifier = modifier
                 .width(300.dp)
-                .padding(8.dp)
-                .height(50.dp),
+                .padding(20.dp),
+                //.height(50.dp),
             maxLines = 5,
             singleLine = false,
             label = { Text("メッセージを入力") }
